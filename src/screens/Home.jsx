0ea86a3d5a54c787d02b06e31d6c71ea122d1a46
jaywomack <womackjw@hotmail.com>
2021-07-28 08:22:17 -0700

    adjusted typrography and transform

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div>
       <Header />
       <main className="min-h-screen bg-custom-color-1">
-        <section id="portfolio" class="section-title ">
-          <h1 class="text-5xl md:text-6xl lg:text-8xl text-custom-ivory text-center lg:text-start ml-12 py-12 lg:py-24 ">
+        <section id="portfolio" className="section-title ">
+          <h1 className="text-5xl md:text-6xl lg:text-8xl text-custom-ivory text-center lg:text-start ml-12 py-12 lg:py-24 ">
             Portfolio
           </h1>
           <div className="flex-row flex-wrap w-100">
@@ -61,34 +61,34 @@ const Home = () => {
           className="flex flex-wrap overflow-hidden ml-12 my-24"
         >
           <div className="w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2">
-            <h2 className="text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               BLOGS
             </h2>
-            <h2 className="text-custom-ivory fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="text-custom-ivory  fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               SHOPIFY
             </h2>
-            <h2 className="text-custom-ivory   fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className=" text-custom-ivory   fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               E-COMMERCE
             </h2>
-            <h2 className="text-custom-ivory   fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className=" text-custom-ivory   fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               SEO
             </h2>
-            <h2 className="text-custom-ivory   fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className=" text-custom-ivory   fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               GRAPHIC DESIGN
             </h2>
-            <h2 className="text-custom-ivory  fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="  fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               REACT
             </h2>
-            <h2 className="text-custom-ivory   fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className=" text-custom-ivory   fw-bold   text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               BOOTSTRAP
             </h2>
-            <h2 className="text-custom-ivory   fw-bold text-custom-ivory text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="  text-custom-ivory  fw-bold  text-4xl md:text-5xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:translate-x-5">
               HTML
             </h2>
-            <h2 className="text-custom-ivory text-4xl lg:text-8xl fw-bold text-custom-ivory text-4xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="text-custom-ivory text-4xl lg:text-8xl fw-bold   text-4xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
               CSS
             </h2>
-            <h2 className="text-custom-ivory text-4xl lg:text-8xl fw-bold text-custom-ivory text-4xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
+            <h2 className="text-custom-ivory text-4xl lg:text-8xl fw-bold text-4xl lg:text-8xl fw-bold transition duration-300 ease-in-out transform hover:-translate-x-5">
               JAVASCRIPT
             </h2>
           </div>
